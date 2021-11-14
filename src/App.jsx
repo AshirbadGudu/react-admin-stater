86e5faa1cc7367311c82d9fc218e14a799338d1a
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-11-14 10:28:04 +0530

    Custom Theme added in MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
+import { ThemeProvider } from "@mui/material";
 import { Loader } from "components/core";
 import { useAppContext } from "contexts";
 import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { PrivateRoutes, PublicRoutes } from "routers";
-
+import CustomTheme from "theme";
 const App = () => {
   const { user } = useAppContext();
 
   return (
     <Suspense fallback={<Loader />}>
-      <BrowserRouter>
-        {user?.email ? <PrivateRoutes /> : <PublicRoutes />}
-      </BrowserRouter>
+      <ThemeProvider theme={CustomTheme}>
+        <BrowserRouter>
+          {user?.email ? <PrivateRoutes /> : <PublicRoutes />}
+        </BrowserRouter>
+      </ThemeProvider>
     </Suspense>
   );
 };
---
+++ src/theme/index.js
@@ -0,0 +1,36 @@
+import { createTheme } from "@mui/material/";
+import { purple } from "@mui/material/colors";
+const CustomTheme = createTheme({
+  palette: {
+    primary: {
+      main: purple[700],
+    },
+  },
+  typography: {
+    fontFamily: ' "Raleway", sans-serif',
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "8px",
+          paddingBottom: "8px",
+          paddingTop: "8px",
+        },
+        contained: {
+          boxShadow: "#6a1b9a3d 0px 8px 16px 0px",
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          borderRadius: "8px",
+          boxShadow: "#6a1b9a3d 0px 8px 16px 0px",
+        },
+      },
+    },
+  },
+});
+
+export default CustomTheme;
