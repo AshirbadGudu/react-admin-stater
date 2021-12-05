5e3dc38929ec3d2ef36f7e54bbb2a6924341b977
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-12-05 10:36:59 +0530

    Loader added in all material tables

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,109 +7,107 @@ import { Container, Typography, Avatar } from "@mui/material";
 const Products = () => {
   const { products } = useProducts();
   const { categories } = useCategories();
-  console.log(categories);
 
   return (
     <section className="py-2">
-      {products && (
-        <MaterialTable
-          title="Products"
-          data={products}
-          columns={[
+      <MaterialTable
+        title="Products"
+        isLoading={!products}
+        data={products || []}
+        columns={[
+          {
+            title: "#",
+            field: "id",
+            render: ({ index }) => index + 1,
+            width: "5%",
+            editable: "never",
+          },
+          {
+            title: "Image",
+            field: "image",
+            render: ({ image }) => (
+              <Avatar
+                src={image}
+                variant="rounded"
+                sx={{ width: 60, height: 60 }}
+              />
+            ),
+            width: "5%",
+          },
+          {
+            title: "Title",
+            field: "title",
+          },
+          {
+            title: "Price",
+            field: "price",
+            render: ({ price }) => formatCurrency(price),
+            width: "5%",
+            type: "numeric",
+          },
+          {
+            title: "Category",
+            field: "category",
+            width: "15%",
+            lookup: categories?.reduce((acc, category) => {
+              acc[category?.name] = category?.name;
+              return acc;
+            }, {}),
+          },
+          { title: "Created At", field: "created_at", editable: "never" },
+        ]}
+        detailPanel={({ rowData }) => (
+          <section className="py-2">
+            <Container>
+              <Typography variant="h6" gutterBottom>
+                {rowData?.title}
+              </Typography>
+              <Typography variant="body1">{rowData?.description}</Typography>
+            </Container>
+          </section>
+        )}
+        options={{
+          exportAllData: true,
+          exportMenu: [
             {
-              title: "#",
-              field: "id",
-              render: ({ index }) => index + 1,
-              width: "5%",
-              editable: "never",
+              label: "Export Products Data In CSV",
+              exportFunc: (cols, data) => ExportCsv(cols, data),
             },
             {
-              title: "Image",
-              field: "image",
-              render: ({ image }) => (
-                <Avatar
-                  src={image}
-                  variant="rounded"
-                  sx={{ width: 60, height: 60 }}
-                />
-              ),
-              width: "5%",
+              label: "Export Products Data In PDF",
+              exportFunc: (cols, data) => ExportPdf(cols, data),
             },
-            {
-              title: "Title",
-              field: "title",
-            },
-            {
-              title: "Price",
-              field: "price",
-              render: ({ price }) => formatCurrency(price),
-              width: "5%",
-              type: "numeric",
-            },
-            {
-              title: "Category",
-              field: "category",
-              width: "15%",
-              lookup: categories?.reduce((acc, category) => {
-                acc[category?.name] = category?.name;
-                return acc;
-              }, {}),
-            },
-            { title: "Created At", field: "created_at", editable: "never" },
-          ]}
-          detailPanel={({ rowData }) => (
-            <section className="py-2">
-              <Container>
-                <Typography variant="h6" gutterBottom>
-                  {rowData?.title}
-                </Typography>
-                <Typography variant="body1">{rowData?.description}</Typography>
-              </Container>
-            </section>
-          )}
-          options={{
-            exportAllData: true,
-            exportMenu: [
-              {
-                label: "Export Products Data In CSV",
-                exportFunc: (cols, data) => ExportCsv(cols, data),
-              },
-              {
-                label: "Export Products Data In PDF",
-                exportFunc: (cols, data) => ExportPdf(cols, data),
-              },
-            ],
-            headerStyle: {
-              backgroundColor: "#f6eef8",
-            },
-            actionsColumnIndex: -1,
-            addRowPosition: "first",
-            selection: true,
-          }}
-          style={{
-            boxShadow: "#6a1b9a3d 0px 8px 16px 0px",
-            borderRadius: "8px",
-          }}
-          editable={{
-            onRowDelete: async (oldData) => {
-              console.log(oldData);
-            },
-            onRowAdd: async (newData) => {
-              console.log(newData);
-            },
-            onRowUpdate: async (newData, oldData) => {
-              console.log(newData, oldData);
-            },
-          }}
-          actions={[
-            {
-              tooltip: "Remove All Selected Products",
-              icon: "delete",
-              onClick: (evt, data) => console.log(data),
-            },
-          ]}
-        />
-      )}
+          ],
+          headerStyle: {
+            backgroundColor: "#f6eef8",
+          },
+          actionsColumnIndex: -1,
+          addRowPosition: "first",
+          selection: true,
+        }}
+        style={{
+          boxShadow: "#6a1b9a3d 0px 8px 16px 0px",
+          borderRadius: "8px",
+        }}
+        editable={{
+          onRowDelete: async (oldData) => {
+            console.log(oldData);
+          },
+          onRowAdd: async (newData) => {
+            console.log(newData);
+          },
+          onRowUpdate: async (newData, oldData) => {
+            console.log(newData, oldData);
+          },
+        }}
+        actions={[
+          {
+            tooltip: "Remove All Selected Products",
+            icon: "delete",
+            onClick: (evt, data) => console.log(data),
+          },
+        ]}
+      />
     </section>
   );
 };
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -14,89 +14,85 @@ const Users = () => {
   const { users } = useUsers();
   return (
     <section className="py-2">
-      {users && (
-        <MaterialTable
-          data={users}
-          title="Users"
-          columns={[
+      <MaterialTable
+        data={users || []}
+        title="Users"
+        columns={[
+          {
+            title: "#",
+            field: "id",
+            render: ({ index }) => index + 1,
+            width: "10%",
+          },
+          {
+            title: "Name",
+            field: "name",
+            render: ({ displayName, email, picture }) => (
+              <>
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar src={picture?.thumbnail} />
+                  </ListItemAvatar>
+                  <ListItemText primary={displayName} secondary={`${email}`} />
+                </ListItem>
+              </>
+            ),
+          },
+          { title: "Email", field: "email", hidden: true, export: true },
+          { title: "Phone", field: "phone" },
+          { title: "Country", field: "country" },
+          {
+            title: "Gender",
+            field: "gender",
+            render: ({ gender }) => (
+              <Button
+                startIcon={gender === "male" ? <Male /> : <Female />}
+                variant="outlined"
+                size="small"
+                color={gender === "male" ? "success" : "info"}
+              >
+                {gender}
+              </Button>
+            ),
+          },
+          { title: "Created At", field: "created_at" },
+        ]}
+        options={{
+          exportAllData: true,
+          exportMenu: [
             {
-              title: "#",
-              field: "id",
-              render: ({ index }) => index + 1,
-              width: "10%",
+              label: "Export Users Data In CSV",
+              exportFunc: (cols, data) => ExportCsv(cols, data),
             },
             {
-              title: "Name",
-              field: "name",
-              render: ({ displayName, email, picture }) => (
-                <>
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar src={picture?.thumbnail} />
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary={displayName}
-                      secondary={`${email}`}
-                    />
-                  </ListItem>
-                </>
-              ),
+              label: "Export Users Data In PDF",
+              exportFunc: (cols, data) => ExportPdf(cols, data),
             },
-            { title: "Email", field: "email", hidden: true, export: true },
-            { title: "Phone", field: "phone" },
-            { title: "Country", field: "country" },
-            {
-              title: "Gender",
-              field: "gender",
-              render: ({ gender }) => (
-                <Button
-                  startIcon={gender === "male" ? <Male /> : <Female />}
-                  variant="outlined"
-                  size="small"
-                  color={gender === "male" ? "success" : "info"}
-                >
-                  {gender}
-                </Button>
-              ),
-            },
-            { title: "Created At", field: "created_at" },
-          ]}
-          options={{
-            exportAllData: true,
-            exportMenu: [
-              {
-                label: "Export Users Data In CSV",
-                exportFunc: (cols, data) => ExportCsv(cols, data),
-              },
-              {
-                label: "Export Users Data In PDF",
-                exportFunc: (cols, data) => ExportPdf(cols, data),
-              },
-            ],
-            selection: true,
-            headerStyle: {
-              backgroundColor: "#f6eef8",
-            },
-            actionsColumnIndex: -1,
-          }}
-          style={{
-            boxShadow: "#6a1b9a3d 0px 8px 16px 0px",
-            borderRadius: "8px",
-          }}
-          editable={{
-            onRowDelete: async (oldData) => {
-              console.log(oldData);
-            },
-          }}
-          actions={[
-            {
-              tooltip: "Remove All Selected Users",
-              icon: "delete",
-              onClick: (evt, data) => console.log(data),
-            },
-          ]}
-        />
-      )}
+          ],
+          selection: true,
+          headerStyle: {
+            backgroundColor: "#f6eef8",
+          },
+          actionsColumnIndex: -1,
+        }}
+        style={{
+          boxShadow: "#6a1b9a3d 0px 8px 16px 0px",
+          borderRadius: "8px",
+        }}
+        editable={{
+          onRowDelete: async (oldData) => {
+            console.log(oldData);
+          },
+        }}
+        actions={[
+          {
+            tooltip: "Remove All Selected Users",
+            icon: "delete",
+            onClick: (evt, data) => console.log(data),
+          },
+        ]}
+        isLoading={!users}
+      />
     </section>
   );
 };
