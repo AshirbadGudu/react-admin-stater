35a3bf4c380c566fdf710c0cd8b12d5d1d60a92d
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-12-05 19:56:24 +0530

    Manage Notifications UI Created

--- src/pages/Settings/Notification.jsx
+++ src/pages/Settings/Notification.jsx
@@ -1,5 +1,57 @@
+import { Done } from "@mui/icons-material";
+import {
+  Button,
+  CardActions,
+  FormControlLabel,
+  FormGroup,
+  Switch,
+  Typography,
+} from "@mui/material";
+
 const Notification = () => {
-  return <>Notification</>;
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Email Notification
+      </Typography>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch defaultChecked />}
+          label="Email me when new order placed"
+        />
+        <FormControlLabel
+          control={<Switch />}
+          label="Email me when a order cancelled"
+        />
+        <FormControlLabel
+          control={<Switch />}
+          label="Allow Email Notifications"
+        />
+      </FormGroup>
+      <Typography variant="h6" gutterBottom>
+        Push Notifications
+      </Typography>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch defaultChecked />}
+          label="Send Push Notifications when new order placed"
+        />
+        <FormControlLabel
+          control={<Switch />}
+          label="Send Push Notifications when a order cancelled"
+        />
+        <FormControlLabel
+          control={<Switch />}
+          label="Allow Push Notifications"
+        />
+      </FormGroup>
+      <CardActions sx={{ justifyContent: "flex-end" }}>
+        <Button variant="contained" startIcon={<Done />}>
+          Save Changes
+        </Button>
+      </CardActions>
+    </>
+  );
 };
 
 export default Notification;
