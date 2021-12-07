a48461de95d75571ceb31e946d6002c23dd84695
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-12-07 08:23:07 +0530

    FAQ hook and mui table created

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export { default as useIsMounted } from "./useIsMounted";
 export { default as useUsers } from "./useUsers";
 export { default as useProducts } from "./useProducts";
 export { default as useCategories } from "./useCategories";
+export { default as useFAQs } from "./useFAQs";
