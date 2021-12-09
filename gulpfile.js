d898911543d457be8b7c6acbba033bec9fa0736b
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-12-09 21:21:54 +0530

    purgecss added to the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function compileSass() {
-  return src("src/scss/index.scss").pipe(sass()).pipe(dest("src/styles"));
+  return src("src/scss/index.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["src/**/*.jsx"] }))
+    .pipe(dest("src/styles"));
 }
 
 function watchSass() {
-  return watch("src/scss/**/*.scss", compileSass);
+  return watch(["src/scss/**/*.scss", "src/**/*.jsx"], compileSass);
 }
 
 exports.default = series(compileSass, watchSass);
